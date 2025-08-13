d0dc1ae5895d72e7717d7e7b115302a23c441e07
TpLink777 <stivengomezmazo7@gmail.com>
2025-08-13 15:35:14 -0500

    Prepares database conection

--- conecionDB.php
+++ conecionDB.php
@@ -1,6 +1,21 @@
 <?php
+    // Parámetros iniciales de la conexión
     $host = "localhost";
     $user = "root";
     $password = "";
     $database = "Medellin_Tours";
+
+    // Opciones para la conexión con la base de datos
+    $options = array(
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'",
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ
+    );
+
+    try {
+        $conection = new PDO("mysql:host=".$host.";dbname=".$database, $user, $password, $options);
+        echo "Conexion exitosa";
+    } catch (PDOException $error) {
+        echo "Error: ".$error->getMessage();
+        die();
+    }
 ?>
