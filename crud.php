fbad4556e03f11cbc8a42ff4a8210f2ea0aa5c30
TpLink777 <stivengomezmazo7@gmail.com>
2025-08-13 15:58:17 -0500

    Adds Names to form inputs and Creates crud file

---
+++ crud.php
@@ -0,0 +1,12 @@
+<?php
+    include "./conecionDB.php";
+
+    /*
+        Inserción de datos a la base de datos.
+    */
+    // Recuperación de los datos del formulario
+    if (isset($_POST)) {
+        echo "Datos enviados correctamente.";
+        
+    }
+?>
